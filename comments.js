e72c61f29b526dbcbcb87b59bfcc6fb2a01af4c5
Bùi Nguyễn Thúy An <66785692+an-bui-nguyen@users.noreply.github.com>
2024-01-13 10:39:27 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,47 +1,55 @@
-// create web server with express
+// Create web server with Node.js
+
+// Import Node.js modules
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// Create web server
 const app = express();
-const path = require('path');
+
+// Set port
 const port = 3000;
 
-// serve static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// set the view engine to ejs
+// Set view engine
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-// index page
-app.get('/', (req, res) => {
-  // render `home.ejs` with the list of posts
-  res.render('pages/index', { posts: posts });
-});
+// Set static files
+app.use(express.static('public'));
 
-// about page
-app.get('/about', (req, res) => {
-  res.render('pages/about');
-});
+// Use body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// start server on port 3000
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
-
-// Array of posts
-const posts = [
+// Store comments
+const comments = [
   {
-    id: 1,
-    title: 'Lorem ipsum dolor sit amet',
-    date: 'September 24, 2019'
+    name: 'John',
+    comment: 'This is a comment',
+    date: '10/10/2020',
   },
   {
-    id: 2,
-    title: 'Consectetur adipiscing elit',
-    date: 'September 26, 2019'
+    name: 'Mary',
+    comment: 'This is another comment',
+    date: '11/10/2020',
   },
-  {
-    id: 3,
-    title: 'Integer molestie lorem at massa',
-    date: 'September 28, 2019'
-  }
 ];
+
+// GET request to home page
+app.get('/', (req, res) => {
+  res.render('home', { comments });
+});
+
+// POST request to home page
+app.post('/', (req, res) => {
+  const newComment = {
+    name: req.body.name,
+    comment: req.body.comment,
+    date: new Date().toLocaleDateString(),
+  };
+  comments.push(newComment);
+  res.redirect('/');
+});
+
+// Start web server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
